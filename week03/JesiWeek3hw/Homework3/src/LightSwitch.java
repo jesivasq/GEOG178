723ae5033ad307be0cc7b57261c997a5f99d9802
Jesi <jesivasq@users.noreply.github.com>
2019-01-29 20:13:35 -0800

    hw3 continued

--- week03/JesiWeek3hw/Homework3/src/LightSwitch.java
+++ week03/JesiWeek3hw/Homework3/src/LightSwitch.java
@@ -1,4 +1,35 @@
 
 public class LightSwitch {
-
+	boolean state; // true means the switch is in the "on" position
+	LightBulb lb;
+	
+	public LightSwitch() {
+		state = false;
+		lb = new LightBulb(state);
+	}
+	
+	
+	public void useSwitch() {
+		state = !state;
+		lb.setState(this.state);
+		lb.reduceLife();
+		if(this.state) {
+			System.out.println("You turned the switch on.");
+		} else {
+			System.out.println("You turned the switch off.");
+		}
+	}
+	
+	public boolean isState() {
+		if(this.lb.isBroken()) {
+			System.out.println("Light bulb is broken.");
+		}
+		if(this.state) {
+			System.out.println("Switch is on.");
+		} else {
+			System.out.println("Switch is off.");
+		}
+		return this.state;
+	}
+	
 }
--- week03/JesiWeek3hw/Homework3/src/Test.java
+++ week03/JesiWeek3hw/Homework3/src/Test.java
@@ -2,8 +2,23 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		// make a light switch
+		LightSwitch ls = new LightSwitch();
+		
+		System.out.println(ls.isState());
+		System.out.println(ls.lb.isState());
+		System.out.println(ls.lb.getLifetime());
+		
+		
+		for(int i = 0; i < 10 ; i++) {
+			ls.useSwitch();
+			
+			System.out.println(ls.isState());
+			System.out.println(ls.lb.isState());
+			System.out.println(ls.lb.getLifetime());
+		}
 	}
 
 }
